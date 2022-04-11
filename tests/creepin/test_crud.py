29a37ea2babe35f9875886ec2381f74e421e6571
scott2b <@scott2b>
2022-04-10 20:02:47 -0500

    still working

--- tests/creepin/test_crud.py
+++ tests/creepin/test_crud.py
@@ -1,42 +1,40 @@
 import os
 import pytest
 from creepin import db
-#from creepin import crud
 from creepin import wiring
-#from creepin.wiring import wire
 from sqlalchemy import text
 from dependency_injector import containers, providers
 
 DB_URI = "sqlite+pysqlite:///:memory:"
-#_db = db.DB(DB_URI, verbose=True)
-#os.environ["CREEPIN_SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-
-#wire()
-
-
-#from . import models
-
 
 _db = db.DB(DB_URI, verbose=True)
 with _db.engine.connect() as con:
     con.execute(text("CREATE TABLE examplemodel (id INT NOT NULL, text VARCHAR(100))"))
 
 
-class Container(containers.DeclarativeContainer):
-    config = providers.Configuration()
-    # Do not use this with the Closing directive
-    db = providers.Factory(_db.Session)
 
-    # For use with Closing.
-    # Use only with scoped_session thread-local scope.
-    closed_db = providers.Resource(_db.get_closed_db)
+#class Container(containers.DeclarativeContainer):
+#    config = providers.Configuration()
+#    # Do not use this with the Closing directive
+#    db = providers.Factory(_db.Session)
+#
+#    # For use with Closing.
+#    # Use only with scoped_session thread-local scope.
+#    closed_db = providers.Resource(_db.get_closed_db)
 
-container = Container()
-wiring.Container = Container
-container.wire(modules=["creepin.crud", ".models"])
-
-#wiring.wire(_db, modules=["creepin.crud", "creepin.models"])
+#Container = containers.DynamicContainer()
+#Container.db = providers.Factory(_db.Session)
+#Container.closed_db = providers.Resource(_db.get_closed_db)
+wiring.wire(_db)
+wiring.Container.wire(modules=["creepin.crud", ".models"])
 from . import models
+
+#wiring.Container = Container
+
+#container = Container()
+#container.wire(modules=["creepin.crud", ".models"])
+#Container.wire(modules=["creepin.crud", ".models"])
+
 
 class TestExampleModel:
 
@@ -44,29 +42,17 @@ class TestExampleModel:
         model = models.ExampleModel()
         with pytest.raises(models.ExampleModel.InvalidState):
             model.save()
-            #with _db.Session() as session:
-            #    model.save(db=session)
-            #    session.commit()
 
     def test_valid_save(self):
         model = models.ExampleModel()
         model.id = 1
         model.save()
-        #with _db.Session() as session:
-        #    model.save(db=session)
-        #    session.commit()
 
     def test_resave(self):
         model = models.ExampleModel()
         model.id = 2
         model.save()
         model.save()
-        #with _db.Session() as session:
-        #    model.save(db=session)
-        #    session.commit()
-        #with _db.Session() as session:
-        #    model.save(db=session)
-        #    session.commit()
 
     def test_dict(self):
         model = models.ExampleModel(text="testing")
